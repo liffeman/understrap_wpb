0590b635919d07f7e0b7d24fe44b2ffc9e2b33a2
Leif Kajrup <leif@kajrup.se>
2020-12-15 17:17:03 +0000

    Basic start

--- inc/template-tags.php
+++ inc/template-tags.php
@@ -15,21 +15,36 @@ if ( ! function_exists( 'understrap_posted_on' ) ) {
 	 * Prints HTML with meta information for the current post-date/time and author.
 	 */
 	function understrap_posted_on() {
-		$time_string = '<time class="entry-date published" datetime="%1$s">%2$s</time>';
+		$time_string = '<time class="entry-date published updated" datetime="%1$s">%2$s</time>';
+		if ( get_the_time( 'U' ) !== get_the_modified_time( 'U' ) ) {
+			$time_string = '<time class="entry-date published" datetime="%1$s">%2$s</time><time class="updated" datetime="%3$s"> (%4$s) </time>';
+		}
 		$time_string = sprintf(
 			$time_string,
 			esc_attr( get_the_date( 'c' ) ),
 			esc_html( get_the_date() ),
+			esc_attr( get_the_modified_date( 'c' ) ),
+			esc_html( get_the_modified_date() )
 		);
 		$posted_on   = apply_filters(
 			'understrap_posted_on',
 			sprintf(
-				'<span class="posted-on">%1$s %2$s</span>',
+				'<span class="posted-on">%1$s <a href="%2$s" rel="bookmark">%3$s</a></span>',
 				esc_html_x( 'Posted on', 'post date', 'understrap' ),
+				esc_url( get_permalink() ),
 				apply_filters( 'understrap_posted_on_time', $time_string )
 			)
 		);
-		echo $posted_on; // phpcs:ignore WordPress.Security.EscapeOutput.OutputNotEscaped
+		$byline      = apply_filters(
+			'understrap_posted_by',
+			sprintf(
+				'<span class="byline"> %1$s<span class="author vcard"> <a class="url fn n" href="%2$s">%3$s</a></span></span>',
+				$posted_on ? esc_html_x( 'by', 'post author', 'understrap' ) : esc_html_x( 'Posted by', 'post author', 'understrap' ),
+				esc_url( get_author_posts_url( get_the_author_meta( 'ID' ) ) ),
+				esc_html( get_the_author() )
+			)
+		);
+		echo $posted_on . $byline; // phpcs:ignore WordPress.Security.EscapeOutput.OutputNotEscaped
 	}
 }
 
