2b076d5c30dfaac72930a4c3eebf8af3bb8c6a63
Leif Kajrup <leif@kajrup.se>
2021-01-27 12:55:42 +0000

    ctp app view

--- single.php
+++ single.php
@@ -26,9 +26,12 @@ $container = get_theme_mod( 'understrap_container_type' );
 				<?php
 				while ( have_posts() ) {
 					the_post();
-					get_template_part( 'loop-templates/content', 'single' );
+					if( get_post_type() == 'app' ) {
+						get_template_part( 'loop-templates/content', 'page' );
+					} else {
+						get_template_part( 'loop-templates/content', 'single' );
 					understrap_post_nav();
-
+					}
 					// If comments are open or we have at least one comment, load up the comment template.
 					if ( comments_open() || get_comments_number() ) {
 						comments_template();
