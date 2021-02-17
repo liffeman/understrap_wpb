2710d248a1efa5ff07bf8c25cf34a34f8180dace
Leif Kajrup <leif@kajrup.se>
2021-02-17 09:27:52 +0000

    add images to rss

--- inc/setup.php
+++ inc/setup.php
@@ -565,3 +565,17 @@ if ( is_plugin_active( 'acf-extended/acf-extended.php' ) ) {
 
 	}
 }
+
+
+//This will prepend your WordPress RSS feed content with the featured image
+add_filter('the_content', 'smartwp_featured_image_in_rss_feed');
+function smartwp_featured_image_in_rss_feed( $content ) {
+  global $post;
+  if( is_feed() ) {
+	if ( has_post_thumbnail( $post->ID ) ){
+	  $prepend = '<div>' . get_the_post_thumbnail( $post->ID, 'large', array( 'style' => 'margin-bottom: 10px;' ) ) . '</div>';
+	  $content = $prepend . $content;
+	}
+  }
+  return $content;
+}
