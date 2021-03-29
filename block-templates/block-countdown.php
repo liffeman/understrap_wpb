65e2657f6e66f523ed23b17a80de372992dee535
Leif Kajrup <leif@kajrup.se>
2021-03-29 12:37:09 +0000

    fix countdown date bugg in safari

--- block-templates/block-countdown.php
+++ block-templates/block-countdown.php
@@ -66,7 +66,7 @@ echo '<a class="anchor" id="' . $section_anchor . '"></a>';
 			echo '</div>';
 			}
 		?>
-			<div id="countdown" class="countdown-content p-0 px-md-5"></div>
+		<div id="countdown" class="countdown-content p-0 px-md-5">	</div>
 
 		<?php if($countdown_weather): ?>
 
@@ -185,8 +185,9 @@ if ($countdown_msg ){
 
 <script>
 	// Set the date we're counting down to
-	//var countDownDate = new Date('Jan 5, 2021 15:37:25').getTime();
+	//var countDownDate = new Date("2021-04-05 14:08:00").getTime();
 	var countDownDate = new Date('<?php echo $countdown_date; ?>').getTime();
+	//var countDownDate = '<?php echo $countdown_now; ?>';
 
 	// Update the count down every 1 second
 	var x = setInterval(function() {
